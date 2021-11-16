b88b6bec339e6a83f522f8ebd801841e067c92cd
1005200 <mdspiker123@gmail.com>
2021-11-16 11:39:36 +0700

    dashboard view with tailwindcss

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,27 +16,6 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
-
-                <!-- my dropdown menu -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <div class="ml-3 ">
-                        <x-mydropdown>
-                            <x-slot name="triger">
-                            <span class="inline-flex rounded-md">
-                                <button type="button">
-                                    blog
-                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                         viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd"
-                                                  d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                                                  clip-rule="evenodd"/>
-                                        </svg>
-                                </button>
-                            </span>
-                            </x-slot>
-                        </x-mydropdown>
-                    </div>
-                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
