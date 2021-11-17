230ecc10932d012a4e39c03ba5e831735dae7ef8
1005200 <mdspiker123@gmail.com>
2021-11-17 15:01:45 +0700

    add update input form 1.1

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -1,56 +1,58 @@
-    <form class="bg-indigo-50 grid md:grid-cols-2 md:gap-4 p-6 rounded-md shadow">
-        <div class="">
-            <label class="text-gray-800" for="">Name :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">Email :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">Phone :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="text-center md:text-left">
-            <label class="text-gray-800" for="">Address :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">City :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">Province/State :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">Country :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="">
-            <label class="text-gray-800" for="">Postal Code :</label>
-            <input
-                class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
-                type="text">
-        </div>
-        <div class="w-1/5  justify-self-center md:justify-self-start">
-            <button class="rounded md:px-0 px-6 py-3 hover:underline text-red-500 font-semibold text-center">Delete</button>
-        </div>
-        <div class="w-1/5 justify-self-center md:justify-self-start ">
-            <button class="bg-indigo-500 px-6 py-3  rounded hover:bg-indigo-600 text-indigo-50 font-semibold text-center">Update</button>
-        </div>
-    </form>
+<form class="bg-indigo-50 grid md:grid-cols-2 md:gap-4 p-6 rounded-md shadow">
+    <div class="">
+        <label class="text-gray-800" for="">Name :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Email :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Phone :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Address :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">City :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Province/State :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Country :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="">
+        <label class="text-gray-800" for="">Postal Code :</label>
+        <input
+            class="bg-indigo-50 w-full border border-indigo-200 rounded focus:outline-none focus:bg-gray-50 focus:ring-opacity-50 focus:ring-indigo-300 focus:border-transparent"
+            type="text">
+    </div>
+    <div class="w-1/5  justify-self-center md:justify-self-start">
+        <button class="rounded md:px-0 px-6 py-3 hover:underline text-red-500 font-semibold text-center">Delete</button>
+    </div>
+    <div class="w-1/5 justify-self-center md:justify-self-start ">
+        <button class="bg-indigo-500 px-6 py-3  rounded hover:bg-indigo-600 text-indigo-50 font-semibold text-center">
+            Update
+        </button>
+    </div>
+</form>
